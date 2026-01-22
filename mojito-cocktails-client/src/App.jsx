9110fe30b54c76ae6395eff47cf5944c45591e01
Jesse Louise Lat <jesselouiselat@gmail.com>
2026-01-22 19:04:09 +0800

    finished about section

--- mojito-cocktails-client/src/App.jsx
+++ mojito-cocktails-client/src/App.jsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 import "./App.css";
 
@@ -15,6 +16,7 @@ function App() {
         <NavBar />
         <Hero />
         <Cocktails />
+        <About />
       </main>
     </>
   );
